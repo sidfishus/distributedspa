a5526902eff5b841f6bd2c36471a6b2002baa71b
Chris.Siddall.1 <Chris.Siddall.1@csp.local>
2019-04-30 17:05:16 +0100

    consent work in progress

--- IdentityServer/ClientApp/Components/Consent/index.jsx
+++ IdentityServer/ClientApp/Components/Consent/index.jsx
@@ -1,53 +1,118 @@
 import * as React from "react";
 import { Fragment } from "react";
-import { Form, Divider, Segment, Checkbox, Grid, Container, Header } from "semantic-ui-react";
+import { Form, Divider, Segment, Checkbox, Grid, Container, Header, Button } from "semantic-ui-react";
 import { ConditionalBold } from "../ConditionalBold";
 
 export class Consent extends React.Component {
     constructor(props) {
+        const { extra: consentModel } = props.prerenderData;
+
         super(props);
+
+        // Copy the consent model in to our state
+        this.state = {
+            identityScopes: this.CopyScopeForState(consentModel.identityScopes),
+            apiScopes: this.CopyScopeForState(consentModel.apiScopes),
+        };
     }
 
     render() {
-        const { prerenderData } = this.props;
-        const { extra: consentModel } = prerenderData;
+        const { identityScopes, apiScopes } = this.state;
+        const { extra: consentModel } = this.props.prerenderData;
 
-        const needScopeDivider=consentModel.identityScopes.length>0 && consentModel.apiScopes.length>0;
+        const needScopeDivider=(identityScopes.length>0 && apiScopes.length>0);
 
         return (
             <Fragment>
                 <br />
                 <br />
                 <Form>
-                    <Container text className="width60Percent">
-                    <Segment placeholder size="huge" raised>
-                        <Header textAlign="center"><u>{consentModel.clientName} is requesting your permission.</u></Header>
-                        <br />
-                        <div>Please uncheck the permissions you do not wish to grant:</div>
-                        <ConsentDivider />
+                    <Container text>
+                        <Segment size="huge" raised>
+                            <Header textAlign="center"><u>{consentModel.clientName} is requesting your permission.</u></Header>
+                            <br />
+                            <div>Please uncheck the permissions you do not wish to grant:</div>
+                            <ConsentDivider />
 
-                        {consentModel.identityScopes.length > 0 &&
-                            <ConstentScopeList
-                                scopeList={consentModel.identityScopes}
-                                title={"Personal Information:"}
-                                keyBase="identity"
-                            />
-                        }
+                            {identityScopes.length > 0 &&
+                                <ConstentScopeList
+                                    scopeList={identityScopes}
+                                    title={"Personal Information:"}
+                                    keyBase="identity"
+                                    fOnChange={(checked,idx) => this.ChangeScopeCheckedStatus(checked,idx,"identityScopes")}
+                                />
+                            }
 
-                        {needScopeDivider && <ConsentDivider horizontal/>}
+                            {needScopeDivider && <ConsentDivider horizontal/>}
 
-                        {consentModel.apiScopes.length > 0 &&
-                            <ConstentScopeList
-                                scopeList={consentModel.apiScopes}
-                                title={"Application Access:"}
-                                keyBase="api"
-                            />
-                        }
-                    </Segment>
+                            {apiScopes.length > 0 &&
+                                <ConstentScopeList
+                                    scopeList={apiScopes}
+                                    title={"Application Access:"}
+                                    keyBase="api"
+                                    fOnChange={(checked,idx) => this.ChangeScopeCheckedStatus(checked,idx,"apiScopes")}
+                                />
+                            }
+
+                            <Grid columns={1}>
+                                <Grid.Column>
+                                    <Button
+                                        primary
+                                        size="big"
+                                        disabled={!this.AllRequiredAccessIsTicked(identityScopes) ||
+                                            !this.AllRequiredAccessIsTicked(apiScopes)}
+                                    >
+                                        Allow
+                                    </Button>
+                                </Grid.Column>
+                            </Grid>
+                        </Segment>
                     </Container>
                 </Form>
             </Fragment>
         );
+    }
+
+    UpdateState(fChange) {
+        this.setState(state => fChange(state));
+    }
+
+    ChangeScopeCheckedStatus(checked, idx, scopeType) {
+        this.UpdateState(state => {
+            let scopeListCopy=state[scopeType];
+            scopeListCopy[idx].checked=checked;
+
+            let rv={};
+            rv[scopeType] = scopeListCopy;
+
+            return rv;
+        });
+    }
+
+    // The index is so we can instantly reference the checkbox in the state later
+    CopyScopeForState(scopeList) {
+        return scopeList.map((scope,idx) => {
+            return {
+                ...scope,
+                checked: true,
+                idx: idx
+            };
+        });
+    }
+
+    AllRequiredAccessIsTicked(scopeList) {
+        let requiredAreAllTicked=true;
+
+        // All required scopes must be checked
+        for(let i=0;i<scopeList.length;++i) {
+            const scope=scopeList[i];
+            requiredAreAllTicked=(!scope.required || scope.checked);
+            if(!requiredAreAllTicked) {
+                break;
+            }
+        }
+
+        return requiredAreAllTicked;
     }
 }
 
@@ -55,10 +120,16 @@ const ConsentDivider = () => {
     return <Divider />;
 };
 
-const ConstentScopeList = ({scopeList, title,keyBase}) => {
+const ConstentScopeList = ({scopeList, title, keyBase, fOnChange}) => {
 
     const scopes=scopeList.map((item,idx) => {
-        return <ConsentScope scope={item} key={`${keyBase}-${idx}`} />
+        return (
+            <ConsentScope
+                scope={item}
+                key={`${keyBase}-${idx}`}
+                fOnChange={checked => fOnChange(checked,idx)}
+            />
+        );
     });
 
     return (
@@ -71,14 +142,18 @@ const ConstentScopeList = ({scopeList, title,keyBase}) => {
     );
 }
 
-const ConsentScope = ({scope}) => {
+const ConsentScope = ({scope, fOnChange}) => {
 
     return (
         <Grid.Row>
-            <Grid.Column width={2}>
-                <Checkbox toggle defaultChecked />
+            <Grid.Column width={3}>
+                <Checkbox
+                    toggle
+                    checked={scope.checked}
+                    onChange={(event,data) => fOnChange(data.checked)}
+                />
             </Grid.Column>
-            <Grid.Column width={8}>
+            <Grid.Column width={13}>
                 <ConditionalBold
                     condition={scope.required}
                 >
